9658bf883b17daf2126cd1c18c1b70f0da90a803
swati kadam <swatimarotikadam@gmail.com>
2026-02-14 18:30:06 +0530

    changed api url railway.app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  // apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://school-app-backend-production.up.railway.app'
+
+
 };
 
